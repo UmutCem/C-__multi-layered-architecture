4814a5b58f558d341b6b9f890b510b0a085a307f
UmutCem <93701663+UmutCem@users.noreply.github.com>
2022-02-01 15:35:48 +0300

    Add files via upload

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class ProductManager
+    {
+        //
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi.");
+
+        }
+
+       // public int Topla(int sayi1, int sayi2) //{ //  return sayi1 + sayi2; // } sonucu başka ortamda da kullanılması gerekiyorsa int tanımlanırızz.
+      // public void Topla2(int sayi1, int sayi2) // { //   Console.WriteLine( sayi1 + sayi2);        // }     // yukarıdaki işlem de sayi 1 ve sayi 2 toplanır ekrana yazdırılır.
+    
+    }
+}
